74ed9d178cc802cfc2eb70b67948a32b4d56c1cf
Chad Olliffe <3538272+odd13@users.noreply.github.com>
2024-08-19 21:04:16 +1000

    Extruder adjustment for correct flow

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -608,7 +608,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600 ,99 }  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 530 }  // default steps per unit for Ultimaker
 
 /**
  * Default Max Feed Rate (mm/s)
